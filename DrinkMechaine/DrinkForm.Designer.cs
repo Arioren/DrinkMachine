cd76cbc9e2436b40036618d48de68d07b2e30731
ari oren <arioren100@gmail.com>
2024-07-25 12:18:28 +0300

    הוספתי דאטה גריד

--- DrinkMechaine/DrinkForm.Designer.cs
+++ DrinkMechaine/DrinkForm.Designer.cs
@@ -44,7 +44,7 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(203, 204);
+            this.dataGridView1.Location = new System.Drawing.Point(179, 213);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.Size = new System.Drawing.Size(240, 150);
